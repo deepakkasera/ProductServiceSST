aa820373c2ca5355f99a5e8c3391733dae1e763f
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-03-31 23:25:49 +0530

    Product Service

---
+++ src/main/java/com/sst/productservicesst/ProductServiceSstApplication.java
@@ -0,0 +1,13 @@
+package com.sst.productservicesst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceSstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceSstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sst/productservicesst/ProductServiceSstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sst.productservicesst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceSstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
