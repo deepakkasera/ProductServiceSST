0846d2bceca9ea5d98e9437b702161ddd3b08caa
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-04-08 17:18:58 +0530

    Database

--- src/main/java/com/sst/productservicesst/models/Category.java
+++ src/main/java/com/sst/productservicesst/models/Category.java
@@ -1,11 +1,15 @@
 package com.sst.productservicesst.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
+    @Id
     private Long id;
     private String title;
     private String description;
--- src/main/java/com/sst/productservicesst/models/Product.java
+++ src/main/java/com/sst/productservicesst/models/Product.java
@@ -1,5 +1,8 @@
 package com.sst.productservicesst.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +12,14 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Product {
+    @Id
     private Long id;
     private String title;
     private String description;
     private Double price;
+    @ManyToOne
     private Category category;
     private String image;
 
